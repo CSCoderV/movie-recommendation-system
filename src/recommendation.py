2a7e31ce532dc369596963ca5a47e778c4a64355
CSCoderV <vsd8623@mavs.uta.edu>
2025-01-18 16:50:17 -0600

    commit 2, errors exsist

--- src/recommendation.py
+++ src/recommendation.py
@@ -1,15 +1,14 @@
-#first importing libraries (using short forms/ psuedonames)
-
-import panda as pd
-df= pd.read_csv('data.csv')
-
-#to see the first 10 rows of the data
+import pandas as pd
+ratings_df = pd.read_csv(r'C:\Users\vedd1\movie-recommendation-system\data\ratings.csv')
+movies_df = pd.read_csv(r'C:\Users\vedd1\movie-recommendation-system\data\movies.csv')
 print("Printing the first 10 rows of the data")
-print(df.head(10))
+print(ratings_df.head(10))
+print(movies_df.head(10))
 
-#to check the general info about the dataset
 print("Printing information about the dataset")
-print(df.info())
+print(ratings_df.info())
+print(movies_df.info())
 
-#basic stats
-print(df.describe())
+#merging the datasets
+merged_df= pd.merge(ratings_df, movies_df, on='moviedId')
+printf(merged_df.head(15))
