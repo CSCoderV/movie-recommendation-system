2ead85d81cbb65ef953aa88634f8fcb68215c1a0
CSCoderV <vsd8623@mavs.uta.edu>
2025-01-26 15:55:17 -0600

    merged data using movieid

--- src/recommendation.py
+++ src/recommendation.py
@@ -8,7 +8,12 @@ print(movies_df.head(10))
 print("Printing information about the dataset")
 print(ratings_df.info())
 print(movies_df.info())
+pd.set_option('display.max_columns', None)  #to see columns in entirety without truncation
+pd.set_option('display.max_rows', None)
 
 #merging the datasets
-merged_df= pd.merge(ratings_df, movies_df, on='moviedId')
-printf(merged_df.head(15))
+merged_df= pd.merge(ratings_df, movies_df, on='movieId')
+print(merged_df.head(15))
+pd.reset_option('display.max_columns')  #resetting to default display settings
+pd.reset_option('display.max_rows')
+
